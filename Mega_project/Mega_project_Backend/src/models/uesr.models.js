f002d0c36bd94ea189253dde9f4bcb827acc6131
Deepak Rai <deepakmegtech@gmail.com>
2025-05-06 08:06:14 +0530

    method created access token, refresh token and temporary token

--- Mega_project/Mega_project_Backend/src/models/uesr.models.js
+++ Mega_project/Mega_project_Backend/src/models/uesr.models.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
-
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 const userSchema = new mongoose.Schema(
   {
     avatar: {
@@ -56,6 +58,50 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorret = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    { _id: this._id, username: this.username, email: this.email },
+    process.env.ACCESS_TOKEN_SCRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    { _id: this._id, username: this.username, email: this.email },
+    process.env.REFRESH_TOKEN_SCRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY },
+  );
+};
+
+userSchema.methods.generateTemporaryToken = function () {
+  const unHashedToken = crypto.randomBytes(32).toString('hex');
+
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(unHashedToken)
+    .digest('hex');
+
+  const tokenExpiry = new Date() + 20 * 60 * 1000; //20minutes
+  return {
+    hashedToken,
+    tokenExpiry,
+    unHashedToken,
+  };
+};
 
 const User = mongoose.model('User', userSchema);
 
